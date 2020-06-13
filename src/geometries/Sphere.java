1070f9fbe29aedbf6901b3e2be838f5314f226ea
sibendav <57369506+sibendav@users.noreply.github.com>
2020-06-13 23:51:54 +0300

    Add files via upload

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,8 +1,20 @@
 package geometries;
 
-import primitives.Point3D;
-import primitives.Vector;
+import premitives.*;
+import premitives.Point3D;
+import premitives.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static premitives.Util.alignZero;
+
+/**
+ * The class: Sphere representing a Sphere in the space
+ * Fields: center point
+ *  implements RadialGeometry abstract class
+ * @author  Simha Ben-David & Tahel Nadav
+ */
 public class Sphere extends RadialGeometry {
 
     public Point3D get_center() {
@@ -11,10 +23,30 @@ public class Sphere extends RadialGeometry {
 
     private Point3D _center;
 
-    Sphere(double R, Point3D center) {
-        super(R);
-        _center=center;
+    /**
+     * sphere ctr with color
+     * @param emissionLight
+     * @param material
+     * @param radius
+     * @param center
+     */
+    public Sphere(Color emissionLight, Material material, double radius, Point3D center) {
+        super(emissionLight, radius, material);
+        this._center = new Point3D(center);
     }
+    /**
+     * ctr with point and radius
+     * @param center Point3D
+     * @param R double = radius
+     */
+    public Sphere(Point3D center,double R) {
+        this(Color.BLACK,new Material(0,0,0),R,center);
+    }
+
+    /**
+     * copy ctr
+     * @param j Sphere
+     */
     public Sphere(Sphere j) {
         super(j.get_radius());
         _center=j.get_center();
@@ -22,9 +54,10 @@ public class Sphere extends RadialGeometry {
 
     @Override
     public Vector getNormal(Point3D p) {
-        return null;
-    }
 
+        Vector v = p.subtract(_center);
+        return v.normalize();
+    }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -39,5 +72,40 @@ public class Sphere extends RadialGeometry {
         return "Sphere{" +
                 "_center=" + _center +
                 '}';
+    }
+
+
+    @Override
+    public List<GeoPoint> findIntsersections(Ray ray) {
+        Point3D p0 = ray.getP();
+        Vector v = ray.getDirection();
+        Vector u;
+
+        try {
+            u = _center.subtract(p0);   // p0 == _center
+        } catch (IllegalArgumentException e) {
+            return List.of(new GeoPoint(this, ray.getTargetPoint(_radius)));
+        }
+        double tm = alignZero(v.dotProduct(u));
+        double dSquared = (tm == 0) ? u.lengthSquared() : u.lengthSquared() - tm * tm;
+        double thSquared = alignZero(_radius * _radius - dSquared);
+
+        if (thSquared <= 0) return null;
+
+        double th = alignZero(Math.sqrt(thSquared));
+        if (th == 0) return null;
+
+        double t1 = alignZero(tm - th);
+        double t2 = alignZero(tm + th);
+        if (t1 <= 0 && t2 <= 0) return null;
+        if (t1 > 0 && t2 > 0) {
+            return List.of(
+                    new GeoPoint(this, ray.getTargetPoint(t1)),
+                    new GeoPoint(this, ray.getTargetPoint(t2))); //P1 , P2
+        }
+        if (t1 > 0)
+            return List.of(new GeoPoint(this, ray.getTargetPoint(t1)));
+        else
+            return List.of(new GeoPoint(this, ray.getTargetPoint(t2)));
     }
 }
--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,27 +1,47 @@
 package geometries;
 
-import primitives.Point3D;
-import primitives.Vector;
-import primitives.Ray;
+import premitives.Point3D;
+import premitives.Vector;
+import premitives.Ray;
 
+import java.util.List;
 import java.util.Objects;
-
+/**
+ * The class: Tube representing a tube in 3D space
+ * Fields: ray
+ * implements RadialGeometry abstract class
+ * @author  Simha Ben-David & Tahel Nadav
+ */
 public class Tube extends RadialGeometry  {
     private Ray _axisRay;
 
-    Tube(double j, Ray r) {
+    /**
+     * ctr with radius and ray
+     * @param j double
+     * @param r Ray
+     */
+    public Tube(double j, Ray r) {
         super(j);
         _axisRay=r;
     }
 
+    /**
+     * ctr with radialGeometry and ray
+     * @param j RadialGeometry
+     * @param r Ray
+     */
     public Tube(RadialGeometry j, Ray r) {
         super(j);
         _axisRay=r;
     }
 
     @Override
+    //imlements of getNormal func
     public Vector getNormal(Point3D p) {
-        return null;
+        double t = _axisRay.getDirection().dotProduct(p.subtract(_axisRay.getP()));
+        Point3D o=new Point3D(_axisRay.getP().add(_axisRay.getDirection().scale(t)));
+        Vector n=new Vector(p.subtract(o)).normalize();
+        return n;
     }
 
     @Override
@@ -42,7 +62,16 @@ public class Tube extends RadialGeometry  {
                 '}';
     }
 
+    /**
+     * get axisRay function
+     * @return
+     */
     public Ray get_axisRay() {
         return _axisRay;
+    }
+
+    @Override
+    public List<GeoPoint> findIntsersections(Ray ray) {
+        return null;
     }
 }
